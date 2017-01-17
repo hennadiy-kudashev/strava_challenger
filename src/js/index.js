11c9ae579a25fcd23d57d7dc5697e65f012a7c54
Yaroslav Kuzyo <yaroslav@jetruby.com>
2017-01-17 16:18:53 +0200

    Add react toolbox support

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+import 'babel-polyfill';
+import React from 'react';
+import { Router, browserHistory } from 'react-router';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import routes from './routes';
+
+// Load styles
+import '../styles/styles.css';
+import '../../node_modules/normalize.css/normalize.css';
+
+const store = configureStore();
+
+render(
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
+    document.getElementById('app')
+);
+
