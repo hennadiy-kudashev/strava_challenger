f5737581bd981eb0d6a11aaf7642ba4b9354d0db
Hennadiy <gena.utyf@gmail.com>
2017-01-20 17:25:02 +0200

    Add authorize params

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login = () => (
     <div className='login'>
-        <a href='https://www.strava.com/oauth/authorize' className='login-btn'></a>
+        <a href='https://www.strava.com/oauth/authorize?client_id=15685&redirect_uri=http://localhost:3000/token_exchange&response_type=code' className='login-btn'></a>
     </div>
 );
 
