73e0c04bfe8cb75446b9cce404f33c101426c831
Yaroslav Kuzyo <yaroslav@jetruby.com>
2017-03-22 17:29:27 +0200

    Add dropdown card

--- src/js/components/layout/Header.js
+++ src/js/components/layout/Header.js
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect  } from 'react-redux';
+import classnames from 'classnames';
 import Logo from './Logo';
 import UserCard from './UserCard';
+import Dropdown from './Dropdown';
 import Error from './Error';
 
-const Header = () => {
-    return (
-        <header className="main-header">
-            <Logo />
-            <Error />
-            <nav className="navbar" role="navigation">
-                <a href="#" className="sidebar-toggle" role="button">
-                    <span className="sr-only">Toggle navigation</span>
-                </a>
-                <div className="navbar-custom-menu">
-                    <ul className="nav navbar-nav">
-                        <li className="dropdown user user-menu">
-                            <UserCard />
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-    );
+class Header extends React.Component  {
+    constructor(props, context) {
+        super(props);
+        this.state = {open: false};
+        this.dropdownToggle = this.dropdownToggle.bind(this);
+    }
+
+    dropdownToggle() {
+        this.setState({
+            open: !this.state.open
+        });
+    }
+
+    render() {
+        const {user} = this.props;
+
+        return (
+            <header className="main-header">
+                <Logo />
+                <Error />
+                <nav className="navbar" role="navigation">
+                    <a href="#" className="sidebar-toggle" role="button">
+                        <span className="sr-only">Toggle navigation</span>
+                    </a>
+                    <div className="navbar-custom-menu">
+                        <ul className="nav navbar-nav">
+                            <li className={classnames('dropdown', 'user', 'user-menu', {'open': this.state.open})}
+                                onClick={this.dropdownToggle}>
+                                <UserCard user={user}/>
+                                <Dropdown user={user}/>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        );
+    }
+}
+
+Header.propTypes = {
+    user: PropTypes.object.isRequired
 };
 
-export default Header;
+function mapStateToProps(state, ownProps) {
+    return {
+        user: state.auth.user
+    };
+}
+
+export default connect(mapStateToProps)(Header);
--- src/js/components/layout/UserCard.js
+++ src/js/components/layout/UserCard.js
@@ -1,29 +1,19 @@
 import React, {PropTypes} from 'react';
-import {connect} from 'react-redux';
 
-class UserCard extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-    }
-    render() {
-        const {avatar, fullName} = this.props.user;
-        return (
-            <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                <img src={avatar} className="user-image" alt="User Image" />
-                <span className="hidden-xs">{fullName}</span>
-            </a>
-        );
-    }
-}
+const UserCard = ({user}) => {
+    const {avatar, fullName} = user;
+
+    return (
+        <a href="#">
+            <img src={avatar} className="user-image" alt="User Image" />
+            <span className="hidden-xs">{fullName}</span>
+        </a>
+    );
+};
 
 UserCard.propTypes = {
     user: PropTypes.object.isRequired
 };
 
-function mapStateToProps(state, ownProps) {
-    return {
-        user: state.auth.user
-    };
-}
+export default UserCard;
 
-export default connect(mapStateToProps)(UserCard);
