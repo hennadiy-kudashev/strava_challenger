22669d6352701856b95e8d105160c470a2d99afc
Yaroslav Kuzyo <yaroslav@jetruby.com>
2017-03-27 15:55:03 +0300

    Add sidebar toggle

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,13 +2,25 @@ import React, { Component, PropTypes } from 'react';
 import Header from './layout/Header';
 import Footer from './layout/Footer';
 import Sidebar from './layout/Sidebar';
+import classnames from 'classnames';
 
 class App extends Component {
+    constructor(props, context) {
+        super(props);
+        this.state = {isSidebar: false};
+        this.toggleSidebar = this.toggleSidebar.bind(this);
+    }
+
+    toggleSidebar(e) {
+        e.preventDefault();
+        this.setState({isSidebar: !this.state.isSidebar});
+    }
+
     render() {
         return (
             <div className="skin-yellow-light" >
-                <div className="wrapper">
-                    <Header />
+                <div className={classnames('wrapper', {'sidebar-open':this.state.isSidebar})}>
+                    <Header toggleSidebar={this.toggleSidebar}/>
                     <Sidebar />
                     <div className="content-wrapper">
                         {this.props.children}
