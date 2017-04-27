65d78f1d359fbaa686749d08d08c231f1f77e763
Hennadiy <gena.utyf@gmail.com>
2017-04-27 18:55:39 +0300

    Changed mongo schema to use _id instead of id

--- src/server/repository/challengeRepository.js
+++ src/server/repository/challengeRepository.js
@@ -1,26 +1,36 @@
 'use strict';
 
+const ObjectId = require('mongodb').ObjectId;
+
 class ChallengeRepository {
     constructor(db) {
         this._db = db;
     }
 
     getAll() {
-        return this._db.collection('challenges').find().toArray();
+        return this._db.collection('challenges').find().toArray().then(items=> {
+            return items.map(item=> {
+                //UI uses id
+                item.id = item._id;
+                return item;
+            });
+        });
     }
 
     get(id) {
-        return this._db.collection('challenges').findOne({id: id});
+        return this._db.collection('challenges').findOne({_id: ObjectId(id)});
     }
 
     addAthlete(challengeID, athlete) {
-        return this._db.collection('challenges').updateOne({id: challengeID}, {$push: {athletes: athlete}});
+        return this._db.collection('challenges').updateOne({_id: ObjectId(challengeID)}, {$push: {athletes: athlete}});
     }
 
     updateUserToken(userID, token) {
-        return this._db.collection('challenges').updateMany({athletes: {$elemMatch: {id: parseInt(userID)}}}, {$set: {
-            "athletes.$.token": token
-        }});
+        return this._db.collection('challenges').updateMany({athletes: {$elemMatch: {id: parseInt(userID)}}}, {
+            $set: {
+                "athletes.$.token": token
+            }
+        });
     }
 }
 module.exports = ChallengeRepository;
