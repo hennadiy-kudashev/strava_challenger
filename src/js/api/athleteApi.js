436901697b06aed70c7ae75420c9760176fce1fe
Hennadiy <gena.utyf@gmail.com>
2017-08-14 12:26:18 +0300

    Requested activities recursive by 200 items.

--- src/js/api/athleteApi.js
+++ src/js/api/athleteApi.js
@@ -21,10 +21,27 @@ class AthleteApi extends StravaApi {
      * Returns activities for given range.
      * @param {Date} before
      * @param {Date} after
-     * @returns {Promise.<TResult>}
+     * @returns {Promise.<Array>}
      */
     getActivities(after, before) {
-        let url = `https://www.strava.com/api/v3/athlete/activities?per_page=200&page=1`;
+        const PAGE_SIZE = 50; //max allowed by Strava.
+        const PAGE_NUMBER = 1;
+        return this._getActivitiesRecursion(after, before, PAGE_SIZE, PAGE_NUMBER, []);
+    }
+    
+    _getActivitiesRecursion(after, before, pageSize, pageNumber, prevActivities){
+        return this._getActivities(after, before, pageSize, pageNumber).then(activities=>{
+            const sumActivities = activities.concat(prevActivities);
+            if (activities.length === pageSize){
+                return this._getActivitiesRecursion(after, before, pageSize, pageNumber + 1, sumActivities);
+            } else {
+                return sumActivities;
+            }
+        });
+    }
+
+    _getActivities(after, before, pageSize, pageNumber){
+        let url = `https://www.strava.com/api/v3/athlete/activities?per_page=${pageSize}&page=${pageNumber}`;
         if (after){
             url += `&after=${after.getTime() / 1000}`;
         }
