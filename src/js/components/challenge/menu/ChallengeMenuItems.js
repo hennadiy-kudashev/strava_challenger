23fa4600a5eb1f74bbddcc59c8ca3a956ef73039
Hennadiy <gena.utyf@gmail.com>
2017-09-04 17:07:11 +0300

    Added CLOSED challenges to menu

---
+++ src/js/components/challenge/menu/ChallengeMenuItems.js
@@ -0,0 +1,28 @@
+import React, {PropTypes} from "react";
+import ChallengeMenuItem from "./ChallengeMenuItem";
+
+const ChallengeMenuItems = ({title, challenges, pathname}) => {
+    const isActive = (id)=> {
+        return pathname.split('/').includes(id);
+    };
+    if (challenges.length == 0) {
+        return null;
+    }
+    return (
+        <ul className="sidebar-menu">
+            <li className="header">{title}</li>
+            {
+                challenges.map(challenge => {
+                    return <ChallengeMenuItem key={challenge.id} active={isActive(challenge.id)} challenge={challenge}/>;
+                })
+            }
+        </ul>);
+};
+
+ChallengeMenuItems.propTypes = {
+    title: PropTypes.string.isRequired,
+    challenges: PropTypes.array.isRequired,
+    pathname: PropTypes.string.isRequired
+};
+
+export default ChallengeMenuItems;
