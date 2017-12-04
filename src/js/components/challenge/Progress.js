a87a099bd3a923e411ea4d25aaf7f253d448d876
Hennadiy <gena.utyf@gmail.com>
2017-12-05 00:00:48 +0200

    Added completed label to Progress component

--- src/js/components/challenge/Progress.js
+++ src/js/components/challenge/Progress.js
@@ -1,17 +1,18 @@
-import React, {PropTypes} from 'react';
-import TotalSummary from '../../logic/totalSummary';
-import PeriodSummary from '../../logic/periodSummary';
-import thresholds from './view/thresholds';
+import React, {PropTypes} from "react";
+import TotalSummary from "../../logic/totalSummary";
+import PeriodSummary from "../../logic/periodSummary";
+import thresholds from "./view/thresholds";
 
 const Progress = ({challenge, user}) => {
     const joinedAthlete = challenge.athletes.find(a=>a.info.id === user.id);
-    if (!joinedAthlete){
+    if (!joinedAthlete) {
         return (<div/>);
     }
     const thresholdCriteria = Object.keys(challenge.criteria.threshold)[0];
     const threshold = challenge.criteria.threshold[thresholdCriteria];
     const achieved = new TotalSummary(joinedAthlete.activities).getByCriterion(thresholdCriteria);
-    const percentage = (achieved/threshold) * 100;
+    const percentage = (achieved / threshold) * 100;
+    const percentageLabel = percentage >= 100 ? `Completed: ${Math.round(percentage)}%` : Math.round(percentage) + '%';
     const periodSummary = new PeriodSummary(challenge.criteria.datetime.after, challenge.criteria.datetime.before);
 
     const label = thresholds[thresholdCriteria].label;
@@ -19,10 +20,13 @@ const Progress = ({challenge, user}) => {
     return (
         <div className="progress-group">
             <span className="progress-text">{label}</span>
-            <span className="progress-number"><b><Component metres={achieved}/></b>/<Component metres={threshold}/></span>
+            <span className="progress-number"><b><Component metres={achieved}/></b>/<Component
+                metres={threshold}/></span>
             <div className="progress">
-                <div className="progress-bar progress-bar-primary progress-bar-striped" style={{width: percentage + '%'}}>{Math.round(percentage) + '%'}</div>
-                &nbsp;<small>{periodSummary.getSummary()}</small>
+                <div className="progress-bar progress-bar-primary progress-bar-striped"
+                     style={{width: (percentage > 100 ? 100: percentage) + '%'}}>{percentageLabel}</div>
+                &nbsp;
+                <small>{periodSummary.getSummary()}</small>
             </div>
         </div>
     );
