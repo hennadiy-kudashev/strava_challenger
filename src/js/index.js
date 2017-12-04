976453acb35d20fec2664f77191bdee0d4c46fe9
Hennadiy <gena.utyf@gmail.com>
2017-12-04 22:38:14 +0200

    Changes admin-lte dep to exact version in order to back bootstrap css.

--- src/js/index.js
+++ src/js/index.js
@@ -11,8 +11,7 @@ import { authenticate } from './logic/auth';
 
 // Load styles
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
-//import '../../node_modules/admin-lte/bootstrap/css/bootstrap.min.css';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/admin-lte/bootstrap/css/bootstrap.min.css';
 import '../../node_modules/admin-lte/dist/css/AdminLTE.min.css';
 import '../../node_modules/admin-lte/dist/css/skins/skin-yellow-light.css';
 import '../resources/styles/styles.scss';
