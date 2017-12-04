76f847f9191e936fa337bea0cfc4f6730a772f4a
Hennadiy <gena.utyf@gmail.com>
2017-12-05 00:44:05 +0200

    Added threshold diff to Total tab.

--- src/js/components/challenge/view/TotalTable.js
+++ src/js/components/challenge/view/TotalTable.js
@@ -1,26 +1,30 @@
 import React, {PropTypes} from "react";
 import UserInfo from "./UserInfo";
 import Table from "../../layout/table/Table";
-import TotalSummary from '../../../logic/totalSummary';
-import {Kilometre, Metre, Time} from './format';
-import BaseView from './BaseView';
+import TotalSummary from "../../../logic/totalSummary";
+import {Kilometre, Metre, Time, Unit} from "./format";
+import BaseView from "./BaseView";
 
 class TotalTable extends BaseView {
     constructor(props, context) {
         super(props, context);
     }
-    
-    render(){
-        const columns = ['Athlete', 'Runs', 'Distance', 'Elev Gain', 'Time'];
 
+    render() {
+        const threshold = this.getThreshold();
+        const columns = ['Athlete', 'Runs', 'Distance', 'Elev Gain', 'Time', `Diff (${threshold.label})`];
+
+        const criterion = this.getThresholdCriterion();
         const rows = super.getSortedAthletes().map((athlete, index)=> {
             const totalSummary = new TotalSummary(athlete.activities);
             return [
                 <UserInfo key={index} userInfo={athlete.info}/>,
                 totalSummary.getRunCount(),
-                <Kilometre key={index} metres={totalSummary.getDistance()} />,
-                <Metre key={index} metres={totalSummary.getElevGain()} />,
-                <Time key={index} seconds={totalSummary.getTime()} />
+                <Kilometre key={index} metres={totalSummary.getDistance()}/>,
+                <Metre key={index} metres={totalSummary.getElevGain()}/>,
+                <Time key={index} seconds={totalSummary.getTime()}/>,
+                <Unit key={index} unit={totalSummary.getByCriterion(criterion)-this.getThresholdValue()}
+                      criterion={criterion} colored/>
             ];
         });
         return (
