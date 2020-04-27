97a32fe03d9c70ffb5a08c6c494f29531b3d8e58
Hennadiy <gena.utyf@gmail.com>
2020-04-27 16:43:06 +0300

    Add table sorting for ActivityTypes table too.

--- src/js/components/challenge/view/ActivityTypesTable.js
+++ src/js/components/challenge/view/ActivityTypesTable.js
@@ -1,26 +1,46 @@
 import React from "react";
 import UserInfo from "./UserInfo";
-import Table from "../../layout/table/Table";
+import Table, { SORT_DIRECTION } from "../../layout/table/Table";
 import TotalSummary from "../../../logic/totalSummary";
 import { Kilometre, Metre, Time } from "./format";
 import BaseView from "./BaseView";
 import Sorter from "../../../logic/sorter";
 import groupByActivityType from "./activityTypes";
+import { THRESHOLD_CRITERION } from "./thresholds";
 
 class ActivityTypesTable extends BaseView {
   constructor(props, context) {
     super(props, context);
-    this.sorter = new Sorter(this.getThresholdCriterion());
+    this.handleSort = this.handleSort.bind(this);
+    this.state = {
+      sortBy: this.getThresholdCriterion(),
+      sortDirection: SORT_DIRECTION.DESC,
+    };
+  }
+
+  handleSort(sortBy, sortDirection) {
+    this.setState({
+      sortBy,
+      sortDirection,
+    });
   }
 
   render() {
-    const columns = ['Athlete', 'Activities', 'Distance', 'Elev Gain', 'Time'];
+    const { sortBy, sortDirection } = this.state;
+
+    const columns = ['Athlete',
+      { key: 'count', title: 'Activities', sortable: true },
+      { key: THRESHOLD_CRITERION.DISTANCE, title: 'Distance', sortable: true },
+      { key: THRESHOLD_CRITERION.EVAL_GAIN, title: 'Elev Gain', sortable: true },
+      { key: THRESHOLD_CRITERION.TIME, title: 'Time', sortable: true },
+    ];
 
     const grouped = groupByActivityType(super.getChallenge().athletes);
     const rows = [];
     Object.keys(grouped).forEach(type => {
       rows.push([type]);
-      grouped[type].sort(this.sorter.getSortFn()).forEach(({ athlete, activities }) => {
+      const sorter = new Sorter(sortBy, sortDirection);
+      grouped[type].sort(sorter.getSortFn()).forEach(({ athlete, activities }) => {
         const totalSummary = new TotalSummary(activities);
         return rows.push([
           <UserInfo key="userInfo" userInfo={athlete.info}/>,
@@ -32,7 +52,13 @@ class ActivityTypesTable extends BaseView {
       });
     });
     return (
-      <Table columns={columns} rows={rows}/>
+      <Table
+        columns={columns}
+        rows={rows}
+        sortBy={sortBy}
+        sortDirection={sortDirection}
+        onSort={this.handleSort}
+      />
     );
   }
 }
